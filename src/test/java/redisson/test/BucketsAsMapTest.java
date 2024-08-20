eb45b232fd07ff959020bd80a892fb2e8c90289c
Ahsan Iqbal <md.ahsan.iqbal@gmai.com>
2024-08-20 17:48:49 +0600

    Buckets as Map Test

--- src/test/java/redisson/test/BucketsAsMapTest.java
+++ src/test/java/redisson/test/BucketsAsMapTest.java
@@ -1,4 +1,20 @@
 package redisson.test;
 
-public class BucketsAsMapTest {
+import org.junit.jupiter.api.Test;
+import org.redisson.client.codec.StringCodec;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class BucketsAsMapTest extends BaseTest{
+
+    @Test
+    public void bucketsAsMapTest() {
+        Mono<Void> mono = this.client.getBuckets(StringCodec.INSTANCE)
+                .get("user:1:name", "user:2:name", "user:3:name", "user:4:name")
+                .doOnNext(System.out::println)
+                .then();
+
+        StepVerifier.create(mono)
+                .verifyComplete();
+    }
 }
