2e47f6b3089c2a9f73d2a3684be39f6e74db9ae3
Ahsan Iqbal <md.ahsan.iqbal@gmai.com>
2024-08-22 14:27:57 +0600

    PubSubTest w/ Topic and PatternTopic

---
+++ src/test/java/redisson/test/PubSubTest.java
@@ -0,0 +1,52 @@
+package redisson.test;
+
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RPatternTopicReactive;
+import org.redisson.api.RTopicReactive;
+import org.redisson.api.listener.PatternMessageListener;
+import org.redisson.client.codec.StringCodec;
+
+public class PubSubTest extends BaseTest {
+
+    //In redis-cli execute 'publish "hello world"' command to see the subscribers receive the message
+
+    @Test
+    public void subscriber1() {
+        RTopicReactive sportsTopic = this.client.getTopic("chat-room:sports", StringCodec.INSTANCE);
+
+        sportsTopic.getMessages(String.class)
+                .doOnNext(System.out::println)
+                .doOnError(System.err::println)
+                .subscribe();
+
+        sleep(120_000);
+    }
+
+    @Test
+    public void subscriber2() {
+        RTopicReactive sportsTopic = this.client.getTopic("chat-room:sports", StringCodec.INSTANCE);
+
+        sportsTopic.getMessages(String.class)
+                .doOnNext(System.out::println)
+                .doOnError(System.err::println)
+                .subscribe();
+
+        sleep(120_000);
+    }
+
+    @Test
+    public void patterSubscriber() {
+        RPatternTopicReactive patternTopic = this.client.getPatternTopic("chat-room:*", StringCodec.INSTANCE);
+
+        patternTopic.addListener(String.class, new PatternMessageListener<String>() {
+            @Override
+            public void onMessage(CharSequence pattern, CharSequence topic, String message) {
+                System.out.println("pattern: " + pattern + " topic: " + topic + " message: " + message);
+            }
+        }).subscribe();
+
+        sleep(120_000);
+    }
+
+
+}
